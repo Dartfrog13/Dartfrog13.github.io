bcdd22fce1c479b9585655d092fea8f7de3d09dd
Dartfrog13 <farrellblake13@gmail.com>
2019-09-14 16:07:04 -0400

    first commit

---
+++ content/my-first-post.md
@@ -0,0 +1,13 @@
+Title: Post Number One
+Date: 2017-11-26 10:01
+Modified: 2017-11-27 12:30
+Category: misc
+Tags: first, misc
+Slug: My-first-post
+Authors: Blake
+Summary: Why I'm here.
+
+This is a blog to document my success and failure. Also going to keep useful sources
+and links here too.
+
+
---
+++ pelicanconf.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Blake'
+SITENAME = 'BAFblog'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'EST'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+##Original vvv
+#FEED_ALL_ATOM = None
+#CATEGORY_FEED_ATOM = None
+#TRANSLATION_FEED_ATOM = None
+#AUTHOR_FEED_ATOM = None
+#AUTHOR_FEED_RSS = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+AUTHOR_FEED_RSS = 'feeds/%s.rss.xml'
+RSS_FEED_SUMMARY_ONLY = False
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+STATIC_Path = ['images']
